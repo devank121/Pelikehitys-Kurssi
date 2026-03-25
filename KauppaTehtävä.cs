bec74c69f742a3c3125f520fac7317a7fe36329a
devank121 <s2401422@edu.bc.fi>
2026-03-25 09:55:46 +0200

    Added notes to the file so i remember everything

--- KauppaTehtävä.cs
+++ KauppaTehtävä.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-enum KarkiTyyppi
+enum KarkiTyyppi  // makes the tip types in to enum
 {
     Puu,
     Teras,
     Timantti
 }
 
-enum PeranTyyppi
+enum PeranTyyppi // makes the back types into enum
 {
     Lehti,
     Kanansulka,
@@ -16,38 +16,38 @@ enum PeranTyyppi
 
 class Nuoli
 {
-    public KarkiTyyppi Karki { get; }
-    public PeranTyyppi Pera { get; }
-    public int Pituus { get; }
+    public KarkiTyyppi Karki { get; }  //gets the types from the enum created before
+    public PeranTyyppi Pera { get; }   // same thing but with the back types
+    public int Pituus { get; }         //gets the height from the user with the constructorgit
 
-    public Nuoli(KarkiTyyppi karki, PeranTyyppi pera, int pituus)
+    public Nuoli(KarkiTyyppi karki, PeranTyyppi pera, int pituus)  //the contruto, gets the answers from the user and saves it in a temporarry varieble
     {
         Karki = karki;
         Pera = pera;
         Pituus = pituus;
     }
 
-    public int PalautaHinta()
+    public int PalautaHinta() //price calculation
     {
-        int hinta = 0;
+        int hinta = 0;   //sets the hinta at 0 from start
 
         // Kärjen hinta
-        switch (Karki)
+        switch (Karki) //switch is basically a "multi-if" its like a shortcut instead of writing 'if' for everthying you can just do it this way.
         {
-            case KarkiTyyppi.Puu: hinta += 3; break;
-            case KarkiTyyppi.Teras: hinta += 5; break;
-            case KarkiTyyppi.Timantti: hinta += 50; break;
+            case KarkiTyyppi.Puu: hinta += 3; break; // adds 3 to the og price which was 0
+            case KarkiTyyppi.Teras: hinta += 5; break; // same thing diff price
+            case KarkiTyyppi.Timantti: hinta += 50; break; // same thing diff price
         }
 
         // Perän hinta
-        switch (Pera)
+        switch (Pera) // same thing but with the back instead of the tip
         {
             case PeranTyyppi.Lehti: hinta += 0; break;
             case PeranTyyppi.Kanansulka: hinta += 1; break;
             case PeranTyyppi.Kotkansulka: hinta += 5; break;
         }
 
-        // Varren hinta (0.05 per cm)
+        // sticks hinta (0.05 per cm)
         hinta += (int)(Pituus * 0.05);
 
         return hinta;
@@ -67,13 +67,14 @@ class Program
         Console.Write("Nuolen pituus sentteinä (60-100): ");
         int pituus = int.Parse(Console.ReadLine());
 
+
         KarkiTyyppi karki = karkiInput switch
         {
             "puu" => KarkiTyyppi.Puu,
             "teräs" => KarkiTyyppi.Teras,
-            "teras" => KarkiTyyppi.Teras, // in case user types without umlaut
+            "teras" => KarkiTyyppi.Teras,
             "timantti" => KarkiTyyppi.Timantti,
-            _ => KarkiTyyppi.Puu
+            _ => KarkiTyyppi.Puu // makes it so if user types anything other than the options it defaults back to puu
         };
 
         PeranTyyppi pera = peraInput switch
@@ -81,11 +82,11 @@ class Program
             "lehti" => PeranTyyppi.Lehti,
             "kanansulka" => PeranTyyppi.Kanansulka,
             "kotkansulka" => PeranTyyppi.Kotkansulka,
-            _ => PeranTyyppi.Lehti
+            _ => PeranTyyppi.Lehti  //same thing as the puu but with leaf
         };
 
-        Nuoli nuoli = new Nuoli(karki, pera, pituus);
+        Nuoli nuoli = new Nuoli(karki, pera, pituus); //makes a new arrow with the users option chose
 
-        Console.WriteLine($"Tämän nuolen hinta on {nuoli.PalautaHinta()} kultarahaa.");
+        Console.WriteLine($"Tämän nuolen hinta on {nuoli.PalautaHinta()} kultarahaa."); // and this returns to the user.
     }
 }
